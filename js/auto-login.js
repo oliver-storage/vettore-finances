6d1b4c3a7ef40486ce02bbed30cfdde6f3833253
jcarlserv <jcarlservicos@gmail.com>
2026-09-11 17:30:33 -0300

    v1.9.79.0
    v1.9.79.0 — Arquivo conversor.html (sub-aba Colaborador Empresa SERVCONTABIL) substituído com CustoUsuarios_-_v1_0_3.html; padrão de cores Vettore aplicado (variáveis CSS --marca, --destaque, --topo-bg, etc), footer "Colaborador Empresa SERVCONTABIL • Desenvolvido por Vettore Finances" posicionado na base do card com separador visual

--- js/auto-login.js
+++ js/auto-login.js
@@ -1,5 +1,5 @@
 /**
- * Vettore Finances - Auto Login v1.9.78.0
+ * Vettore Finances - Auto Login v1.9.79.0
  * Cria usuário automático e faz login sem senha
  */
 
--- js/importador-boletos.js
+++ js/importador-boletos.js
@@ -1,5 +1,5 @@
 /**
- * Vettore Finances - Módulo Boletos v1.9.78.0
+ * Vettore Finances - Módulo Boletos v1.9.79.0
  */
 
 let BOLETOS_CACHE = [];
